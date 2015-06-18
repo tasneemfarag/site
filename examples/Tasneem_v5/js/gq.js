f14bc6ba08254df9f931d9419b5b4f9632ab7928
Tasneem Farag <tasneem.a.farag@gmail.com>
2015-06-17 22:44:20 -0400

    change some things
    1-Make all the index element responsive
    2-Show surah list selected by default
    3-Make Juz and page view
    4-Make a simple data for surah, Juz, page
    5-toggle the play and pause button
    6-hide the close button

---
+++ examples/Tasneem_v5/js/gq.js
@@ -0,0 +1,172 @@
+$(document).ready(function() {
+	/* console.log("hghkg");
+	$( ".modal-dialog" ).dialog({
+	title: "Global Quran",
+	width: 1000,
+	modal: true,
+	opacity: 0.1
+	});*/
+
+	function allButtonsNotSelecetedView() {
+		if ($("div#surah").is(":visible") == false && $("div#juz").is(":visible") == false && $("div#page").is(":visible") == false) {
+			console.log("hello");
+		}
+	}
+
+	$("i.icon-gear.icon-3x").click(function() {
+		$(".modal-dialog").dialog({
+			width: 1000,
+			modal: true,
+			opacity: 0.1
+		});
+	});
+	$("#btn-surah").click(function() {
+		if ($("div#surah").is(":visible") == true) {
+			$("#btn-surah").toggleClass("active");
+			$("div#surah").hide("slow");
+		} else {
+			$("div#surah").show("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+			$("div.Al-Faatiha").hide("slow");
+			$("div.Al-Baqara").hide("slow");
+			$("div.Aal-Imran").hide("slow");
+			$("div.An-Nisaa").hide("slow");
+			$("div.juz-2").hide("slow");
+			$("div.page-16").hide("slow");
+		}
+	});
+	$("#btn-juz").click(function() {
+		if ($("div#juz").is(":visible") == true) {
+			$("div#juz").hide("slow");
+		} else {
+			$("div#juz").show("slow");
+			$("div#surah").hide("slow");
+			$("div#page").hide("slow");
+			$("div.Al-Faatiha").hide("slow");
+			$("div.Al-Baqara").hide("slow");
+			$("div.Aal-Imran").hide("slow");
+			$("div.An-Nisaa").hide("slow");
+			$("div.juz-2").hide("slow");
+			$("div.page-16").hide("slow");
+		}
+	});
+	$("#btn-page").click(function() {
+		if ($("div#page").is(":visible") == true) {
+			$("div#page").hide("slow");
+		} else {
+			$("div#page").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div.Al-Faatiha").hide("slow");
+			$("div.Al-Baqara").hide("slow");
+			$("div.Aal-Imran").hide("slow");
+			$("div.An-Nisaa").hide("slow");
+			$("div.juz-2").hide("slow");
+			$("div.page-16").hide("slow");
+		}
+	});
+	$(".btn-Al-Faatiha").click(function() {
+		if ($("div.Al-Faatiha").is(":visible") == true) {
+			$("div.Al-Faatiha").hide("slow");
+		} else {
+			$("div.Al-Faatiha").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$(".btn-Al-Baqara").click(function() {
+		if ($("div.Al-Baqara").is(":visible") == true) {
+			$("div.Al-Baqara").hide("slow");
+		} else {
+			$("div.Al-Baqara").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$(".btn-Aal-Imran").click(function() {
+		if ($("div.Aal-Imran").is(":visible") == true) {
+			$("div.Aal-Imran").hide("slow");
+		} else {
+			$("div.Aal-Imran").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$(".btn-An-Nisaa").click(function() {
+		if ($("div.An-Nisaa").is(":visible") == true) {
+			$("div.An-Nisaa").hide("slow");
+		} else {
+			$("div.An-Nisaa").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$("#btn-juz-2").click(function() {
+		if ($("div.juz-2").is(":visible") == true) {
+			$("div.juz-2").hide("slow");
+		} else {
+			$("div.juz-2").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$("#btn-page-16").click(function() {
+		if ($("div.page-16").is(":visible") == true) {
+			$("div.page-16").hide("slow");
+		} else {
+			$("div.page-16").show("slow");
+			$("div#surah").hide("slow");
+			$("div#juz").hide("slow");
+			$("div#page").hide("slow");
+		}
+	});
+	$("div#play").click(function() {
+		if ($("div#play > button > span").hasClass('glyphicon-pause')) {
+			$("div#play > button > span").switchClass("glyphicon-pause", "glyphicon-play", 4000, "easeInOutQuad");
+		} else {
+			$("div#play > button > span").switchClass("glyphicon-play", "glyphicon-pause", 4000, "easeInOutQuad");
+		}
+	});
+	$("ul.list-menu.list-unstyled.list-inline>li>a.no-underline").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('a.selected').removeClass("selected");
+		$(this).addClass("selected");
+		var index = $(this).index();
+		$("ul.list-menu.list-unstyled.list-inline>li>a.no-underline").removeClass("selected");
+		$("ul.list-menu.list-unstyled.list-inline>li>a.no-underline").eq(index).addClass("selected");
+	});
+	$("div.setting-tab-menu>div.list-group>a").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('a.active').removeClass("active");
+		$(this).addClass("active");
+		var index = $(this).index();
+		$("div.setting-tab>div.setting-tab-content").removeClass("active");
+		$("div.setting-tab>div.setting-tab-content").eq(index).addClass("active");
+	});
+});
+/*
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=175325295868821&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+! function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0],
+		p = /^http:/.test(d.location) ? 'http' : 'https';
+	if (!d.getElementById(id)) {
+		js = d.createElement(s);
+		js.id = id;
+		js.src = p + '://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js, fjs);
+	}
+}(document, 'script', 'twitter-wjs');
+*/
